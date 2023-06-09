545f06597d11f69a4e9418df4cac9725dd7dc0d0
Iasmim Rodrigues <iasmimrodrigues1301@gmail.com>
2023-06-09 19:20:19 -0300

    Adicionado função de filtrar cards

--- scripts.js
+++ scripts.js
@@ -1,12 +1,21 @@
-// dropzone - cards
-function log(message) {
-  console.log("> " + message)
-}
-
 // função para mudar do dark para o light mode
 function toggleMode() {
   const html = document.documentElement
   html.classList.toggle("dark-mode")
+}
+
+function addFontSize(amountToAdd) {
+  const style = window.getComputedStyle(document.body)
+  let percent = Number(style.getPropertyValue("--font-size").split("%")[0])
+  percent += amountToAdd
+  document.documentElement.style.setProperty("--font-size", percent + "%")
+}
+
+function dimFontSize(amountToAdd) {
+  const style = window.getComputedStyle(document.body)
+  let percent = Number(style.getPropertyValue("--font-size").split("%")[0])
+  percent -= amountToAdd
+  document.documentElement.style.setProperty("--font-size", percent + "%")
 }
 
 // parte do drag and drop
@@ -23,19 +32,15 @@ function dragstart() {
   // log('Card: start')
   dropzones.forEach((dropzone) => dropzone.classList.add("highlights"))
 
-  this.classList.add('is-dragging')
+  this.classList.add("is-dragging")
 }
 
-function drag() {
-  // log("Card: is gragging")
-}
+function drag() {}
 
 function dragend() {
-  // log("Card: stop drag")
   dropzones.forEach((dropzone) => dropzone.classList.remove("highlights"))
 
   this.classList.remove("is-dragging")
-
 }
 
 // local onde vai soltar os cartões
@@ -47,24 +52,128 @@ dropzones.forEach((dropzone) => {
   dropzone.addEventListener("drop", drop)
 })
 
-function dragenter() {
-  // log("dropzone: enter in zone")
-}
+function dragenter() {}
 
 function dragover() {
-  // log("dropzone: over")
-  this.classList.add('over')
+  this.classList.add("over")
 
-  const cardBeingDragged = document.querySelector('.is-dragging')
+  const cardBeingDragged = document.querySelector(".is-dragging")
   this.appendChild(cardBeingDragged)
 }
 
 function dragleave() {
-  // log("dropzone: leave")
   this.classList.remove("over")
-
 }
 
-function drop() {
-  // log("dropzone: dropped")
+function drop() {}
+
+// modal cadastrar cards
+
+const button = document.getElementById("buttonOpen")
+const modal = document.querySelector("dialog")
+const buttonClose = document.getElementById("buttonClose")
+
+button.onclick = function () {
+  modal.showModal()
+}
+
+buttonClose.onclick = function () {
+  modal.close()
+}
+
+// inserir cartão
+function inserirCard() {
+  let titulo = document.getElementById("titulo").value
+  let descricao = document.getElementById("descricao").value
+  let tag = document.getElementById("tag").value
+
+  document.getElementById("todo").innerHTML +=
+    '<div class="card" draggable="true"> <h3>' +
+    titulo +
+    "</h3> <p>" +
+    descricao +
+    '</p> <div class="tags"> <span>' +
+    tag +
+    "</span> </div> </div>"
+
+  modal.close()
+
+  const cards = document.querySelectorAll(".card")
+  const dropzones = document.querySelectorAll(".cards")
+
+  cards.forEach((card) => {
+    card.addEventListener("dragstart", dragstart)
+    card.addEventListener("drag", drag)
+    card.addEventListener("dragend", dragend)
+  })
+
+  function dragstart() {
+    // log('Card: start')
+    dropzones.forEach((dropzone) => dropzone.classList.add("highlights"))
+
+    this.classList.add("is-dragging")
+  }
+
+  function drag() {}
+
+  function dragend() {
+    dropzones.forEach((dropzone) => dropzone.classList.remove("highlights"))
+
+    this.classList.remove("is-dragging")
+  }
+
+  cardsFilter = document.querySelectorAll(".card");
+}
+
+/*function inserirTag() {
+  document.getElementById("insertTag").innerHTML += 
+    '<input type="text" id="tag" />'
+  // const tag = document.getElementById("tag").value;
+
+  // document.getElementsByClassName("tags").innerHTML +=
+  //   '<span>' + tag + '</span>';
+}*/
+
+// Filtrar cartões
+const filterElements = document.querySelector(".filter .input-wrapper input")
+let cardsFilter = document.querySelectorAll(".card")
+
+console.log(cardsFilter)
+
+filterElements.addEventListener("input", filterCards)
+
+// function filterCards() {
+//   if (filterElements.value != '') {
+//     for (let card of cardsFilter) {
+//       let title = card.querySelector("h3")
+//       title = title.textContent.toLowerCase()
+//       let filterText = filterElements.value.toLowerCase();
+
+//       if (!title.includes(filterText)) {
+//         console.log("ta entrando")
+//         card.style.display = "none"
+//       } else {
+//         card.style.display = "block"
+//       }
+//     }
+//   } else {
+//     for (let card of cardsFilter) {
+//       card.style.display = "block"
+//     }
+//   }
+// }
+
+function filterCards() {
+  const filterText = filterElements.value.toLowerCase()
+
+  for (let card of cardsFilter) {
+    let isMatch = false
+    const cardContent = card.textContent.toLowerCase()
+
+    if (cardContent.includes(filterText)) {
+      isMatch = true
+    }
+
+    card.style.display = isMatch ? "block" : "none"
+  }
 }
